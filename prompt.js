fe00ac34bdcd0e788ca2571c227f99fa470d3e09
Truptichaudhari22 <71192997+Truptichaudhari22@users.noreply.github.com>
2023-04-16 19:23:32 +0530

    Delete prompt.js

--- prompt.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-    var age = prompt("Enter your age:");
-    if (age >= 18) {
-        alert("You are old enough to drive");
-} else {
-        alert("wait till you are 18")
-    }
-}
